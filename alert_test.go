742373ae0b8b26623507dd99a751371b12f7495e
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-06-16 09:54:12 +0200

    support multiple assertions

--- alert_test.go
+++ alert_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	. "github.com/datadog/e2e/pkg/e2e"
 	. "github.com/datadog/e2e/pkg/integrations"
+	"time"
 
 	_ "github.com/datadog/stratus-red-team/pkg/stratus/loader" // Note: This import is needed
 	"testing"
@@ -15,30 +16,31 @@ func TestAlerts(t *testing.T) {
 	WithTimeout(10 * time.Minute).
 	Run(t)*/
 
-	ssh, err := NewSSHCommandExecutor("test-box", "", "")
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	//TODO: Orchestration
 	// MVP: 1 goroutine per scenario
 	// Problem: Many duplicated queries to the Datadog API
 	// Solution 2:
 	// Pass 1: detonate everything
 	// Pass 2: filter through all alerts
+
+	/*ssh, err := NewSSHCommandExecutor("test-box", "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
 	WhenDetonating(NewCommandDetonator(ssh, "curl http://169.254.169.254 --connect-timeout 5")).
 		Expect(DatadogSecuritySignal("EC2 Instance Metadata Service Accessed via Network Utility")).
 		Run(t)
 
 	WhenDetonating(NewCommandDetonator(ssh, `cp /bin/bash /tmp/java; /tmp/java -c "curl 1.1.1.1"`)).
 		Expect(DatadogSecuritySignal("Java process spawned shell/utility")).
-		Run(t)
+		Run(t)*/
 
 	// TODO: CWS container executor
 
-	/*WhenDetonating(StratusRedTeamTechnique("aws.initial-access.console-login-without-mfa")).
-	Expect(DatadogSecuritySignal("AWS Console login without MFA").WithSeverity("medium")).
-	WithTimeout(10 * time.Minute).
-	Run(t)*/
+	WhenDetonating(StratusRedTeamTechnique("aws.initial-access.console-login-without-mfa")).
+		Expect(DatadogSecuritySignal("AWS Console login without MFA").WithSeverity("medium")).
+		Expect(DatadogSecuritySignal("An IAM user was created")).
+		WithTimeout(10 * time.Minute).
+		Run(t)
 
 }
--- pkg/e2e/scenario.go
+++ pkg/e2e/scenario.go
@@ -50,23 +50,38 @@ func (m *ScenarioBuilder) Run(t *testing.T) {
 		return
 	}
 
-	for {
-		if m.Timeout > 0 && time.Now().After(start.Add(m.Timeout)) {
-			t.Error("timed out waiting for alert")
-		}
-		hasAlert, err := m.Scenario.Assertions[0].HasExpectedAlert(detonationUid)
+	// Go over each
+	remainingAssertions := m.Scenario.Assertions
+	defer func() {
+		err := m.Scenario.Assertions[0].Cleanup(detonationUid)
 		if err != nil {
-			t.Error(err)
+			t.Log("warning: failed to clean up generated signals: " + err.Error())
 		}
-		if hasAlert {
-			timeSpentStr := strconv.Itoa(int(time.Since(start).Seconds()))
-			log.Printf("Confirmed that the expected signal was created in Datadog (took %s seconds). Archiving it\n", timeSpentStr)
-			err = m.Scenario.Assertions[0].Cleanup(detonationUid) // TODO only supports 1 assertion
+	}() // TODO: this shouldn't be specific to a single assertion
+
+	for len(remainingAssertions) > 0 && (m.Timeout == 0 || !time.Now().After(start.Add(m.Timeout))) {
+		for i := range remainingAssertions {
+			assertion := remainingAssertions[i]
+			hasAlert, err := assertion.HasExpectedAlert(detonationUid)
 			if err != nil {
-				t.Log("warning: failed to clean up generated signals: " + err.Error())
+				t.Error(err)
 			}
-			return
+			if hasAlert {
+				timeSpentStr := strconv.Itoa(int(time.Since(start).Seconds()))
+				log.Printf("Confirmed that the expected signal was created in Datadog (took %s seconds). Archiving it\n", timeSpentStr)
+
+				// Remove assertion. c.f.https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
+				numAssertions := len(remainingAssertions)
+				remainingAssertions[i] = remainingAssertions[numAssertions-1]
+				remainingAssertions = remainingAssertions[:numAssertions-1]
+			}
 		}
 		time.Sleep(interval)
+	}
+
+	if len(remainingAssertions) > 0 {
+		t.Error(strconv.Itoa(len(remainingAssertions)) + " assertions did not pass")
+	} else {
+		t.Log("All assertions passed")
 	}
 }
